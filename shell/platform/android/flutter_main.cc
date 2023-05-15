05dc370f855dddb02eca44d2715bf1e6fc2bdc38
Bryan Oltman <bryan@shorebird.dev>
2023-05-15 15:44:34 -0400

    format

--- shell/platform/android/flutter_main.cc
+++ shell/platform/android/flutter_main.cc
@@ -205,14 +205,15 @@ void FlutterMain::Init(JNIEnv* env,
 
 #if FLUTTER_RELEASE
   if (shorebirdYaml) {
-    std::string shorebird_yaml = fml::jni::JavaStringToString(env, shorebirdYaml);
+    std::string shorebird_yaml =
+        fml::jni::JavaStringToString(env, shorebirdYaml);
     std::string version_string = fml::jni::JavaStringToString(env, version);
     long version_code = versionCode;
     ConfigureShorebird(android_cache_path, settings, shorebird_yaml,
-                      version_string, version_code);
+                       version_string, version_code);
   } else {
-    __android_log_print(ANDROID_LOG_ERROR,
-        "Flutter", "No Shorebird.yaml provided");
+    __android_log_print(ANDROID_LOG_ERROR, "Flutter",
+                        "No Shorebird.yaml provided");
   }
 #endif
 
