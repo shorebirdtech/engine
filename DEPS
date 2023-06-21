698dcc5a736ba12db2026af0ea1b6529fe70d6b2
Bryan Oltman <bryanoltman@gmail.com>
2023-06-21 17:04:45 -0400

    fix: don't attempt to init shorebird if we aren't running in a Flutter app

--- DEPS
+++ DEPS
@@ -23,7 +23,7 @@ vars = {
   'dart_sdk_revision': '69fdf1867dc65697420febd94d061e6ab90e5d7c',
   'dart_sdk_git': 'https://github.com/shorebirdtech/dart-sdk.git',
   'updater_git': 'https://github.com/shorebirdtech/updater.git',
-  'updater_rev': 'bce15087879a06d7e7f4ce37c1a54dd13bc4b8b1',
+  'updater_rev': 'c627ad79a185941f44100292fcf04cbabd4c11d0',
 
   # WARNING: DO NOT EDIT canvaskit_cipd_instance MANUALLY
   # See `lib/web_ui/README.md` for how to roll CanvasKit to a new version.
--- shell/common/shorebird.cc
+++ shell/common/shorebird.cc
@@ -1,6 +1,7 @@
 
 #include "flutter/shell/common/shorebird.h"
 
+#include <filesystem>
 #include <optional>
 #include <vector>
 
@@ -39,6 +40,28 @@ extern "C" __attribute__((weak)) unsigned long getauxval(unsigned long type) {
 }
 #endif
 
+/// Checks whether the last item in the list of application library paths has a
+/// filename that matches what we expect to see for a Flutter app on the current
+/// platform.
+///
+/// On Android, this is "libapp.so".
+/// On iOS, this is "App".
+bool IsRunningApp(std::vector<std::string> libapp_paths) {
+  std::filesystem::path path(libapp_paths.back());
+  std::string filename = path.filename().string();
+
+// Check for the filename we expect on the current platform.
+#if defined(__ANDROID__)
+  return filename == "libapp.so";
+#elif defined(__APPLE__)
+  return filename == "App";
+#endif
+
+  // If we aren't on Android or iOS, we're in an unfamiliar environment and
+  // shouldn't assume we're running a Flutter app.
+  return false;
+}
+
 void ConfigureShorebird(std::string cache_path,
                         flutter::Settings& settings,
                         const std::string& shorebird_yaml,
@@ -66,8 +89,24 @@ void ConfigureShorebird(std::string cache_path,
     }
     // Do not modify application_library_path or c_strings will invalidate.
 
+    // original_libapp_paths is a set of fallback paths to libapp.so.
+    // The first item in original_libapp_paths will be "libapp.so".
+    // This fails on some old versions of Android, and will fall back to the
+    // second path, which is the absolute path to the libapp.so file.
+    // FIXME: resolve the full path from "libapp.so" and pass that to Rust.
+    // See updater.rs for more details.
     app_parameters.original_libapp_paths = c_paths.data();
     app_parameters.original_libapp_paths_size = c_paths.size();
+
+    // If we don't believe that we're running a full Flutter app, warn the user
+    // and don't try to initialize Shorebird.
+    if (!IsRunningApp(settings.application_library_path)) {
+      FML_LOG(WARNING)
+          << "Shorebird updater: application library detected is not "
+             "libapp.so, not running shorebird_init (detected "
+          << settings.application_library_path.front() << ").";
+      return;
+    }
 
     // shorebird_init copies from app_parameters and shorebirdYaml.
     shorebird_init(&app_parameters, shorebird_yaml.c_str());
