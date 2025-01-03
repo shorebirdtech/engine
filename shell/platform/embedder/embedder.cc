c0e4596f34c794ef0f83863bbe671a799dfc33db
BOLTMAN-WINDOWS\bryan <bryan@shorebird.dev>
2025-01-03 14:25:53 -0500

    Formatting

--- shell/platform/embedder/embedder.cc
+++ shell/platform/embedder/embedder.cc
@@ -2314,12 +2314,12 @@ FlutterEngineResult FlutterEngineInitialize(size_t version,
   // some platforms (i.e., Windows) need to to swap out the app path before
   // this point.
   // Begin shorebird
-  #if FML_OS_MACOSX
+#if FML_OS_MACOSX
   if (args->shorebird_args.shorebird_yaml_contents) {
     settings.application_library_path.push_back(args->shorebird_args.app_path);
     flutter::ConfigureShorebird(args->shorebird_args, settings);
   }
-  #endif
+#endif
   // End shorebird
 
   // Create the engine but don't launch the shell or run the root isolate.
